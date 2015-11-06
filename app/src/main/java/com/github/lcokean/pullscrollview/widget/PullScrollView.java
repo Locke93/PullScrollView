c47dd478ba0f2895120b9446d4b46e723152259c
pengjian <qq1072174696@hotmail.com>
2015-11-06 15:54:49 +0800

    Initial commit

---
+++ app/src/main/java/com/github/lcokean/pullscrollview/widget/PullScrollView.java
@@ -0,0 +1,303 @@
+package com.github.lcokean.pullscrollview.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.ScrollView;
+
+/**
+ * Created by pengjian on 2015/11/5 0005.
+ */
+public class PullScrollView extends ScrollView {
+
+    /**
+     * The max scroll height.
+     */
+    private static final int MAX_SCROLL_HEIGHT = 400;
+    /**
+     * 回滚的时间
+     */
+    private static final int SCROLL_DURATION = 350;
+    /**
+     * Damping, the smaller the greater the resistance
+     */
+    private static final float SCROLL_RATIO = 2.5f;
+
+    private View mChildRootView;
+
+    private float mTouchY;
+    private boolean mTouchStop = false;
+
+    /**
+     * 平滑滚动的Runnable
+     */
+    private SmoothScrollRunnable mSmoothScrollRunnable;
+
+    public PullScrollView(Context context) {
+        super(context);
+
+        init();
+    }
+
+    public PullScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init();
+    }
+
+    public PullScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        init();
+    }
+
+    private void init() {
+        // set scroll mode
+        setOverScrollMode(OVER_SCROLL_NEVER);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        if (getChildCount() > 0) {
+            // when finished inflating from layout xml, get the first child view
+            mChildRootView = getChildAt(0);
+        }
+        super.onFinishInflate();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            mTouchY = ev.getY();
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (null != mChildRootView) {
+            doTouchEvent(ev);
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    private void doTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (null != mSmoothScrollRunnable) {
+                    mSmoothScrollRunnable.stop();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (getScrollYValue() != 0) {
+                    mTouchStop = true;
+                    smoothScrollTo(0);
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                float nowY = ev.getY();
+                int deltaY = (int) (mTouchY - nowY);
+                mTouchY = nowY;
+                if (isNeedMove()) {
+                    int offset = getScrollYValue();
+                    if (offset < MAX_SCROLL_HEIGHT && offset > -MAX_SCROLL_HEIGHT) {
+                        setScrollBy(0, (int) (deltaY / SCROLL_RATIO));
+                        mTouchStop = false;
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private boolean isNeedMove() {
+        int viewHeight = mChildRootView.getMeasuredHeight();
+        int scrollHeight = getHeight();
+        int offset = viewHeight - scrollHeight;
+        int scrollY = getScrollY();
+
+        return scrollY == 0 || scrollY == offset;
+    }
+
+    /**
+     * 设置滚动位置
+     *
+     * @param x 滚动到的x位置
+     * @param y 滚动到的y位置
+     */
+    private void setScrollTo(int x, int y) {
+        mChildRootView.scrollTo(x, y);
+    }
+
+    /**
+     * 设置滚动的偏移
+     *
+     * @param x 滚动x位置
+     * @param y 滚动y位置
+     */
+    private void setScrollBy(int x, int y) {
+        mChildRootView.scrollBy(x, y);
+    }
+
+    /**
+     * 得到当前Y的滚动值
+     *
+     * @return 滚动值
+     */
+    private int getScrollYValue() {
+        return mChildRootView.getScrollY();
+    }
+
+
+    /**
+     * 得到平滑滚动的时间，派生类可以重写这个方法来控件滚动时间
+     *
+     * @return 返回值时间为毫秒
+     */
+    protected long getSmoothScrollDuration() {
+        return SCROLL_DURATION;
+    }
+
+    /**
+     * 平滑滚动
+     *
+     * @param newScrollValue 滚动的值
+     */
+    private void smoothScrollTo(int newScrollValue) {
+        smoothScrollTo(newScrollValue, getSmoothScrollDuration(), 0);
+    }
+
+    /**
+     * 平滑滚动
+     *
+     * @param newScrollValue 滚动的值
+     * @param duration       滚动时候
+     * @param delayMillis    延迟时间，0代表不延迟
+     */
+    private void smoothScrollTo(int newScrollValue, long duration, long delayMillis) {
+        if (null != mSmoothScrollRunnable) {
+            mSmoothScrollRunnable.stop();
+        }
+
+        int oldScrollValue = this.getScrollYValue();
+        boolean post = (oldScrollValue != newScrollValue);
+        if (post) {
+            mSmoothScrollRunnable = new SmoothScrollRunnable(oldScrollValue, newScrollValue, duration);
+        }
+
+        if (post) {
+            if (delayMillis > 0) {
+                postDelayed(mSmoothScrollRunnable, delayMillis);
+            } else {
+                post(mSmoothScrollRunnable);
+            }
+        }
+    }
+
+    /**
+     * 实现了平滑滚动的Runnable
+     *
+     * @author Li Hong
+     * @since 2013-8-22
+     */
+    final class SmoothScrollRunnable implements Runnable {
+        /**
+         * 动画效果
+         */
+        private final Interpolator mInterpolator;
+        /**
+         * 结束Y
+         */
+        private final int mScrollToY;
+        /**
+         * 开始Y
+         */
+        private final int mScrollFromY;
+        /**
+         * 滑动时间
+         */
+        private final long mDuration;
+        /**
+         * 是否继续运行
+         */
+        private boolean mContinueRunning = true;
+        /**
+         * 开始时刻
+         */
+        private long mStartTime = -1;
+        /**
+         * 当前Y
+         */
+        private int mCurrentY = -1;
+
+        /**
+         * 构造方法
+         *
+         * @param fromY    开始Y
+         * @param toY      结束Y
+         * @param duration 动画时间
+         */
+        public SmoothScrollRunnable(int fromY, int toY, long duration) {
+            mScrollFromY = fromY;
+            mScrollToY = toY;
+            mDuration = duration;
+            mInterpolator = new DecelerateInterpolator();
+        }
+
+        @Override
+        public void run() {
+            /**
+             * If the duration is 0, we scroll the view to target y directly.
+             */
+            if (mDuration <= 0) {
+                setScrollTo(0, mScrollToY);
+                return;
+            }
+
+            /**
+             * Only set mStartTime if this is the first time we're starting,
+             * else actually calculate the Y delta
+             */
+            if (mStartTime == -1) {
+                mStartTime = System.currentTimeMillis();
+            } else {
+
+                /**
+                 * We do do all calculations in long to reduce software float
+                 * calculations. We use 1000 as it gives us good accuracy and
+                 * small rounding errors
+                 */
+                final long oneSecond = 1000;    // SUPPRESS CHECKSTYLE
+                long normalizedTime = (oneSecond * (System.currentTimeMillis() - mStartTime)) / mDuration;
+                normalizedTime = Math.max(Math.min(normalizedTime, oneSecond), 0);
+
+                final int deltaY = Math.round((mScrollFromY - mScrollToY)
+                        * mInterpolator.getInterpolation(normalizedTime / (float) oneSecond));
+                mCurrentY = mScrollFromY - deltaY;
+
+                setScrollTo(0, mCurrentY);
+            }
+
+            // If we're not at the target Y, keep going...
+            if (mContinueRunning && mScrollToY != mCurrentY) {
+                PullScrollView.this.postDelayed(this, 16);// SUPPRESS CHECKSTYLE
+            }
+        }
+
+        /**
+         * 停止滑动
+         */
+        public void stop() {
+            mContinueRunning = false;
+            removeCallbacks(this);
+        }
+    }
+}
